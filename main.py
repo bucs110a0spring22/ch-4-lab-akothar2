4cb9763c3efcc69e56b0c00236d0a999f0551ea8
Archit Kothari <98614998+akothar2@users.noreply.github.com>
2022-03-07 15:09:46 +0000

    started part b

--- main.py
+++ main.py
@@ -5,29 +5,47 @@ import math
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
-y = math.sin(math.radians(270))
-print(y)
-z = math.sin(math.radians(360))
-print(z)
+xmin = -360
+xmax = 360
+ymin = -1
+ymax = 1
 
-wn = turtle.Screen()
-fred = turtle.Turtle()
-fred.goto(50,50)
-fred.goto(-100,60)
-fred.goto(-100,-75)
-fred.goto(250,-75)
-fred.goto(0,0)
-wn.exitonclick()
+def drawSineCurve(dart):
+  dart.color("green")
+  dart.down()
+  for x in range (xmin, xmax + 1, 1):
+    dart.goto(x, math.sin(math.radians(x)))
+    dart.up()
+    dart.goto(xmin, 0)
 
+def setupWindow(wn):
+  wn.setworldcoordinates(xmin, ymin, xmax, ymax)
+  wn.bgcolor("white")
 
+def setupAxis(dart):
+  dart.up()
+  dart.color("blue")
+  for z in (ymin, ymax):
+    dart.goto(xmin, 0)
+    dart.down()
+    dart.goto(xmax, 0)
+  dart.goto(xmin - 25, 0)
+  dart.write("X")
 
+  for z in range (xmin, xmax + 1, 50):
+    if z==0 or z==xmin or z==xmax:
+            dart.color("black")
+    else:
+      dart.color("grey")
+      dart.goto(z, ymax)
+      dart.down()
+      dart.goto(z, ymin)
+      dart.up()
+      dart.goto(0, ymax)
+      dart.write("Y")
 
-
-
-
-
-
-
+    
+      
 ##########  Do Not Alter Any Code Past Here ########
 def main():
     #Part A
@@ -42,8 +60,6 @@ def main():
     setupAxis(dart)
     dart.speed(0)
     drawSineCurve(dart)
-    drawCosineCurve(dart)
-    drawTangentCurve(dart)
     wn.exitonclick()
 
 main()
